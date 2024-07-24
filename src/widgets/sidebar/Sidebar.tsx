1e85725c009ba3799b742cdee7ec795662b21567
Oops14 <ups14q@gmail.com>
2024-07-25 00:13:13 +0300

    styles for sidebar

--- src/widgets/sidebar/Sidebar.tsx
+++ src/widgets/sidebar/Sidebar.tsx
@@ -1,30 +1,50 @@
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined'
+import EditCalendarIcon from '@mui/icons-material/EditCalendar'
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural'
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined'
+import NotificationsIcon from '@mui/icons-material/Notifications'
 
 const Sidebar = () => {
     return (
         <div className={'main-sidebar'}>
-            <ul className={"sidebar-menu"}>
-                <li className={"active"}>
+            <ul className={'sidebar-menu'}>
+                <li className={'active'}>
                     <a href="#">
+                        <span>
+                            <FaceRetouchingNaturalIcon />
+                        </span>
                         All Notes
                     </a>
                 </li>
                 <li>
                     <a href="#">
+                        <span>
+                            <NotificationsIcon />
+                        </span>
                         Reminders
                     </a>
                 </li>
                 <li>
                     <a href="#">
+                        <span>
+                            <EditCalendarIcon />
+                        </span>
                         Edit Labels
                     </a>
                 </li>
                 <li>
                     <a href="#">
+                        <span>
+                            <Inventory2OutlinedIcon />
+                        </span>
                         Archives
                     </a>
                 </li>
                 <li>
                     <a href="#">
+                        <span>
+                            <DeleteOutlineOutlinedIcon />
+                        </span>
                         Trash
                     </a>
                 </li>

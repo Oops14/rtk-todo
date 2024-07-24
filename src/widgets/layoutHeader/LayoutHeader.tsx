37374dd7135308469727543e6a7c54e4df8bbeae
Oops14 <ups14q@gmail.com>
2024-07-24 23:58:07 +0300

    styles for header

--- src/widgets/layoutHeader/LayoutHeader.tsx
+++ src/widgets/layoutHeader/LayoutHeader.tsx
@@ -1,7 +1,36 @@
+import logo from '../../assets/7.jpeg'
+import SettingsIcon from '@mui/icons-material/Settings'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import SearchIcon from '@mui/icons-material/Search'
+
 const LayoutHeader = () => {
     return (
         <header className={'header-main'}>
-            <h3>Header</h3>
+            <div className="header-inner">
+                <div className={'search'}>
+                    <div className="s-icon">
+                        <SearchIcon />
+                    </div>
+                    <input type="text" placeholder={'Search for note...'} />
+                </div>
+                <div className="header-account-info">
+                    <div className="date">
+                        <span>15:15</span>
+                        <span>26 FEB</span>
+                    </div>
+                    <div className="header-settings">
+                        <div className="reminder">
+                            <NotificationsIcon />
+                        </div>
+                        <div className="settings">
+                            <SettingsIcon />
+                        </div>
+                        <div className="account-logo">
+                            <img src={logo} alt="lgo" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
